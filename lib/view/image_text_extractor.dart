d19841ce7bdf2594cee3df40d0146b11c4616f8e
Hussain-Shadab <141557949+Hussain-Shadab@users.noreply.github.com>
2025-04-18 00:36:36 +0500

    updated

---
+++ lib/view/image_text_extractor.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+class ImageTextExtractor extends StatefulWidget {
+  const ImageTextExtractor({super.key});
+
+  @override
+  State<ImageTextExtractor> createState() => _ImageTextExtractorState();
+}
+
+class _ImageTextExtractorState extends State<ImageTextExtractor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
